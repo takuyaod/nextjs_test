4f401a1c1ccbfbf9a126069675af925f112d2ecf
takuyaoda <88299141+takuyaod@users.noreply.github.com>
2022-10-07 17:54:07 +0900

    テスト成功

--- src/lib/__tests__/example.test.ts
+++ src/lib/__tests__/example.test.ts
@@ -2,7 +2,7 @@ import { square } from '../example'
 
 describe('square', () => {
   it('計算結果が正しいこと', () => {
-    const expected = 6
-    expect(square(3)).toStrictEqual(expected)
+    const expected = 4
+    expect(square(2)).toStrictEqual(expected)
   })
 })
